c3b022373e3b5e90659212ead71bdbe316f9d0f3
Adam P. Krieger <apkriege@svsu.edu>
2018-10-24 16:17:13 -0400

    added homepage container

---
+++ src/containers/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class HomePage extends React.Component {
+    render() { 
+        return (
+            <h1>HOME TESTING</h1>
+        )
+    }
+}
+
+export default HomePage
